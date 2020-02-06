6ece53c86034ae3d4c7691eee52b6e61fe584d4b
Alejandro <asirstudent401@gmail.com>
2020-02-06 12:15:56 +0100

    Saludando a Ruben Morgan

--- HiMr/src/hello.java
+++ HiMr/src/hello.java
@@ -2,8 +2,11 @@
 public class hello {
 
 	public static void main(String[] args) {
+		
 		// TODO Apéndice de método generado automáticamente
 		System.out.println("Hello");
+		System.out.println("Easy boy");
+		System.out.println("Hi mr");
 	}
 
 }
